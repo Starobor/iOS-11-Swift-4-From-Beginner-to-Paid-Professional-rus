b21aa2274ef1d57b01fe892338c3ec8095d81cdd
Citizen <citizen@Mac-Admin.local>
2018-08-16 17:20:46 +0300

    additional

--- additional/Task_3-4(additional).playground/Contents.swift
+++ additional/Task_3-4(additional).playground/Contents.swift
@@ -12,7 +12,7 @@ import UIKit
 // #3
 class TimeToNow {
     
-  	  let year, month, day: String
+    let year, month, day: String
     let dateNow = Int(Date().timeIntervalSince1970)
     let date: Int
     
@@ -47,11 +47,19 @@ print(TimeToNow(year: "1985", month: "05", day: "05").getYears())
 
 // #4
 func sumTwoNumbers (num1: String, num2: String) -> String {
-    let max = (Int(num1.count)+Int(num2.count)+abs(Int(num1.count)-Int(num2.count)))/2
+    let max = getMax(max1: num1.count, max2: num2.count)
     let numArr1 = numArr(num: num1, max: max)
     let numArr2 = numArr(num: num2, max: max)
     let result = addArr(numArr1: numArr1.reversed(), numArr2: numArr2.reversed())
-    return String(result.reversed())
+    return result
+}
+
+func getMax (max1: Int, max2: Int) -> Int{
+    if max1 > max2 {
+        return max1
+    } else {
+        return max2
+    }
 }
 
 func numArr(num: String, max: Int) -> [Int] {
@@ -62,37 +70,25 @@ func numArr(num: String, max: Int) -> [Int] {
     }
     
     if num.count <= max {
-        for _ in 0...max - num.count + 1{
+        for _ in 0...max - num.count + 1 {
             numInArr.insert(0, at: 0)
         }
     }
-    
-    print(numInArr)
     return numInArr
 }
 
 func addArr (numArr1: [Int], numArr2: [Int]) -> String {
     var numberArr1 = numArr1
     var numberArr2 = numArr2
-    print(numArr1)
-    print(numArr2)
     var result = String()
     var forSave = 0
     for index in 0 ..< numberArr1.count - 1 {
         var sum = 0
         
         
-            let sumParied = getSumParied(num1: numberArr1[index], num2: numberArr2[index])
-            sum = sumParied.0
-            forSave = sumParied.1
-        
-        
-        
-        if ((numberArr1[index] % 2) == 1 || (numberArr1[index] % 2) == 1) {
-            print("1")
-        }
-        
-        print("\(numberArr1[index+1]) \(numberArr1[index+1] % 2) \(forSave)")
+        let sumParied = getSumParied(num1: numberArr1[index], num2: numberArr2[index])
+        sum = sumParied.0
+        forSave = sumParied.1
         
         if forSave != 0 && (numberArr1[index+1] % 2) == 0 {
             numberArr1[index+1] = numberArr1[index+1] ^ forSave
@@ -104,6 +100,14 @@ func addArr (numArr1: [Int], numArr2: [Int]) -> String {
         result.append(String(sum))
         forSave = 0
     }
+    result = String(result.reversed())
+    for index in result {
+        if index == "0" {
+            result.removeFirst()
+        } else {
+            break
+        }
+    }
     return result
 }
 
@@ -111,48 +115,320 @@ func getSumParied(num1: Int, num2: Int) -> (Int, Int){
     var sum = 0
     var forSave = 0
     
-   
-    if num1 == num2 {
-        sum = num2 << 1
-    } else if (num1 % 2) == 0 || (num2 % 2) == 0  {
-        sum = num1 ^ num2
-    } else if (num1 % 2) == 1 || (num2 % 2) == 1 {
-        
-    }
-
-    if sum >= 10 {
-        switch sum {
-        case 10:
-            sum = 0
-        case 11:
+    switch num1 {
+    case 0:
+        sum = num2
+    case 1:
+        switch num2 {
+        case 0:
             sum = 1
-        case 12:
+        case 1:
             sum = 2
-        case 13:
+        case 2:
             sum = 3
-        case 14:
+        case 3:
             sum = 4
-        case 15:
+        case 4:
             sum = 5
-        case 16:
+        case 5:
             sum = 6
-        case 17:
+        case 6:
             sum = 7
-        case 18:
+        case 7:
             sum = 8
-        case 19:
+        case 8:
             sum = 9
+        case 9:
+            sum = 0
+            forSave = 1
         default:
             print("somthing wrong")
         }
-        forSave = 1
+    case 2:
+        switch num2 {
+        case 0:
+            sum = 2
+        case 1:
+            sum = 3
+        case 2:
+            sum = 4
+        case 3:
+            sum = 5
+        case 4:
+            sum = 6
+        case 5:
+            sum = 7
+        case 6:
+            sum = 8
+        case 7:
+            sum = 9
+        case 8:
+            sum = 0
+            forSave = 1
+        case 9:
+            sum = 1
+            forSave = 1
+        default:
+            print("somthing wrong")
+        }
+    case 3:
+        switch num2 {
+        case 0:
+            sum = 3
+        case 1:
+            sum = 4
+        case 2:
+            sum = 5
+        case 3:
+            sum = 6
+        case 4:
+            sum = 7
+        case 5:
+            sum = 8
+        case 6:
+            sum = 9
+        case 7:
+            sum = 0
+            forSave = 1
+        case 8:
+            sum = 1
+            forSave = 1
+        case 9:
+            sum = 2
+            forSave = 1
+        default:
+            print("somthing wrong")
+        }
+    case 4:
+        switch num2 {
+        case 0:
+            sum = 4
+        case 1:
+            sum = 5
+        case 2:
+            sum = 6
+        case 3:
+            sum = 7
+        case 4:
+            sum = 8
+        case 5:
+            sum = 9
+        case 6:
+            sum = 0
+            forSave = 1
+        case 7:
+            sum = 1
+            forSave = 1
+        case 8:
+            sum = 2
+            forSave = 1
+        case 9:
+            sum = 3
+            forSave = 1
+        default:
+            print("somthing wrong")
+        }
+    case 5:
+        switch num2 {
+        case 0:
+            sum = 5
+        case 1:
+            sum = 6
+        case 2:
+            sum = 7
+        case 3:
+            sum = 8
+        case 4:
+            sum = 9
+        case 5:
+            sum = 0
+            forSave = 1
+        case 6:
+            sum = 1
+            forSave = 1
+        case 7:
+            sum = 2
+            forSave = 1
+        case 8:
+            sum = 3
+            forSave = 1
+        case 9:
+            sum = 4
+            forSave = 1
+        default:
+            print("somthing wrong")
+        }
+    case 6:
+        switch num2 {
+        case 0:
+            sum = 6
+        case 1:
+            sum = 7
+        case 2:
+            sum = 8
+        case 3:
+            sum = 9
+        case 4:
+            sum = 0
+            forSave = 1
+        case 5:
+            sum = 1
+            forSave = 1
+        case 6:
+            sum = 2
+            forSave = 1
+        case 7:
+            sum = 3
+            forSave = 1
+        case 8:
+            sum = 4
+            forSave = 1
+        case 9:
+            sum = 5
+            forSave = 1
+        default:
+            print("somthing wrong")
+        }
+    case 7:
+        switch num2 {
+        case 0:
+            sum = 7
+        case 1:
+            sum = 8
+        case 2:
+            sum = 9
+        case 3:
+            sum = 0
+            forSave = 1
+        case 4:
+            sum = 1
+            forSave = 1
+        case 5:
+            sum = 2
+            forSave = 1
+        case 6:
+            sum = 3
+            forSave = 1
+        case 7:
+            sum = 4
+            forSave = 1
+        case 8:
+            sum = 5
+            forSave = 1
+        case 9:
+            sum = 6
+            forSave = 1
+        default:
+            print("somthing wrong")
+        }
+    case 8:
+        switch num2 {
+        case 0:
+            sum = 8
+        case 1:
+            sum = 9
+        case 2:
+            sum = 0
+            forSave = 1
+        case 3:
+            sum = 1
+            forSave = 1
+        case 4:
+            sum = 2
+            forSave = 1
+        case 5:
+            sum = 3
+            forSave = 1
+        case 6:
+            sum = 4
+            forSave = 1
+        case 7:
+            sum = 5
+            forSave = 1
+        case 8:
+            sum = 6
+            forSave = 1
+        case 9:
+            sum = 7
+            forSave = 1
+        default:
+            print("somthing wrong")
+        }
+    case 9:
+        switch num2 {
+        case 0:
+            sum = 9
+        case 1:
+            sum = 0
+            forSave = 1
+        case 2:
+            sum = 1
+            forSave = 1
+        case 3:
+            sum = 2
+            forSave = 1
+        case 4:
+            sum = 3
+            forSave = 1
+        case 5:
+            sum = 4
+            forSave = 1
+        case 6:
+            sum = 5
+            forSave = 1
+        case 7:
+            sum = 6
+            forSave = 1
+        case 8:
+            sum = 7
+            forSave = 1
+        case 9:
+            sum = 8
+            forSave = 1
+        default:
+            print("somthing wrong")
+        }
+    case 10:
+        switch num2 {
+        case 0:
+            sum = 0
+            forSave = 1
+        case 1:
+            sum = 1
+            forSave = 1
+        case 2:
+            sum = 2
+            forSave = 1
+        case 3:
+            sum = 3
+            forSave = 1
+        case 4:
+            sum = 4
+            forSave = 1
+        case 5:
+            sum = 5
+            forSave = 1
+        case 6:
+            sum = 6
+            forSave = 1
+        case 7:
+            sum = 7
+            forSave = 1
+        case 8:
+            sum = 8
+            forSave = 1
+        case 9:
+            sum = 9
+            forSave = 1
+        default:
+            print("somthing wrong")
+        }
+    default:
+        print("somthing wrong")
     }
     return (sum, forSave)
 }
 
-print(sumTwoNumbers(num1: "12", num2: "8"))
-
-
-
+print(sumTwoNumbers(num1: "1070", num2: "1000"))
 
 
